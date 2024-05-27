f9b24b5b9cc918828f2e7b40f2cdcbee01f0bb06
Vanillin <ivan.elistrat@mail.ru>
2024-05-27 20:42:08 +0300

    Some changing LinCode5-2

--- LabMAZ/LabMAZ/LinCode5-2.cs
+++ LabMAZ/LabMAZ/LinCode5-2.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LabMAZ
 {
@@ -10,116 +7,87 @@ namespace LabMAZ
     {
         public LinCode5_2() { }
 
-        int[,] ReadMatrix = new int[,]
+        int[,] MatrixG = new int[2, 5] //любые два вектора (независимых)
         {
-            {1,0,1,1,1 },
-            {0,1,0,1, 1 }
+            { 1, 1, 0, 1, 0 },
+            { 0, 1, 1, 0, 1 }
+        //    { 1, 0, 1, 1, 1 },
+        //    { 0, 1, 0, 1, 1 }
         };
-        int[,] ToSindrom = new int[,]
+        int[,] MatrixH = new int[3, 5] //матрица H, любые три вектора (независимых) //высчитывается из G
         {
-            {1,0,1,0,0 },
-            {1,1,0,1,0 },
-            {1,1,0,0,1 }
+            { 1, 1, 1, 0, 0 },
+            { 1, 0, 0, 1, 0 },
+            { 0, 0, 1, 0, 1 }
+        //    { 1, 0, 1, 0, 0 },
+        //    { 1, 1, 0, 1, 0 },
+        //    { 1, 1, 0, 0, 1 }
         };
-        int[,] Smeznosti = new int[,]
+        Dictionary<string, int> Syndromes = new Dictionary<string, int>
         {
-            {0,0,0,0,0 },
-            {1,0,0,0,0 },
-            {0,1,0,0,0 },
-            {0,0,1,0,0 },
-            {0,0,0,1,0 },
-            {0,0,0,0,1 },
-            {1,0,0,0,1 },
-            {1,0,0,1,0 },
+            { "000", 0 },
+            { "110", 1 },
+            { "100", 2 },
+            { "101", 3 },
+            { "010", 4 },
+            { "001", 5 },
+            { "111", -1 },
+            { "011", -1 }
+        //    { "000", 0 },
+        //    { "111", 1 },
+        //    { "011", 2 },
+        //    { "100", 3 },
+        //    { "010", 4 },
+        //    { "001", 5 },
+        //    { "110", -1 },
+        //    { "101", -1 }
         };
-        int[,] Sindroms;
-        public int[] Method(int a, int b)
+        public int[] MethodCodir(int a, int b)
         {
-            Sindroms = new int[8, 3];
-            for (int i = 0; i < 8; i++)
-            {
-                int[] current = new int[3];
-                for (int j = 0; j < 5; j++)
-                {
-                    if (Smeznosti[i,j] == 1)
-                    {
-                        for (int k = 0;k < 3; k++)
-                        {
-                            current[k] += ToSindrom[k,j];
-                        }
-                    }
-                }
-                for (int j = 0; j < 3; j++)
-                {
-                    Sindroms[i,j] = current[j] % 2;
-                }
-            }
-
-            int[] code = new int[5];
+            int[] code = new int[5]; //закодированное слово
             for (int i = 0; i < 5; i++)
             {
-                code[i] = (a * ReadMatrix[0,i] + b * ReadMatrix[1,i]) % 2;
+                code[i] = (a * MatrixG[0, i] + b * MatrixG[1, i]) % 2;
             }
-
             return code;
         }
         public int[] FixCode(int[] code)
         {
-            int[] curSindrom = new int[3];
+            int[] curSindrom = new int[3]; //высчитывает синдром
             for (int j = 0; j < 5; j++)
             {
                 if (code[j] == 1)
                 {
                     for (int k = 0; k < 3; k++)
                     {
-                        curSindrom[k] += ToSindrom[k, j];
+                        curSindrom[k] += MatrixH[k, j];
+                        if (curSindrom[k] == 2) 
+                            curSindrom[k] = 0;
                     }
                 }
             }
-            for (int j = 0; j < 3; j++)
-            {
-                curSindrom[j] = curSindrom[j] % 2;
-            }
 
-            int indexCurrent = -1;
-            for (int i = 0; i < 8; i++)
-            {
-                bool Equals = true;
-                for (int j = 0; j < 3; j++)
-                {
-                    if (curSindrom[j] != Sindroms[i, j])
-                    {
-                        Equals = false;
-                        break;
-                    }
-                }
-                if (Equals)
-                {
-                    indexCurrent = i;
-                    break;
-                }
-            }
+            string sindrom = $"{curSindrom[0]}{curSindrom[1]}{curSindrom[2]}";
+            int indexCurrent = Syndromes[sindrom];
+            if (indexCurrent == 0) return code; //ошибки нет
+            if (indexCurrent < 0) throw new Exception("Слишком много ошибок, нельзя восстановить");
 
-            int[] oshibka = new int[5];
-            for (int i = 0; i < 5; i++)
+            // Заменяем символ в указанной позиции
+            if (code[indexCurrent-1] == 1)
             {
-                oshibka[i] = Smeznosti[indexCurrent, i];
+                code[indexCurrent-1] = 0;
             }
-
-            int[] result = new int[5];
-            for (int i = 0; i < 5; i++)
+            else
             {
-                result[i] = code[i] - oshibka[i];
-                if (result[i] < 0) result[i] += 2;
+                code[indexCurrent-1] = 1;
             }
-
-            return result;
+            return code;
         }
 
         public void Codirov(string Text, out string zacodirText, out Dictionary<string, string> Diction, out int WeightZacodirText)
         {
             string[] ints = Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            int[] result = Method(int.Parse(ints[0]), int.Parse(ints[1]));
+            int[] result = MethodCodir(int.Parse(ints[0]), int.Parse(ints[1]));
             zacodirText = "";
             foreach (var v in result)
             {
@@ -131,7 +99,7 @@ namespace LabMAZ
 
         public void Decodirov(string zacodirText, Dictionary<string, string> Diction, out string Text)
         {
-            string[] ints = zacodirText.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            string[] ints = zacodirText.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             int[] ints1 = new int[ints.Length];
             for (int i = 0; i < ints.Length; i++)
                 ints1[i] = int.Parse(ints[i]);
